f189ab9ab6daf4bd84a010f1cbae919b2e15ba21
Jake Bills <jakebills1@gmail.com>
2019-03-08 10:15:27 -0700

    colorize output

--- magic_number.rb
+++ magic_number.rb
@@ -1,7 +1,7 @@
-
 require 'to_words'
+require 'colorize'
 def prompt
-  puts "Please input a number or type quit"
+  puts "Please input a number or type quit".colorize(:light_blue)
   input = gets.strip
   while input != "quit"
     magic_number(input.to_i)
@@ -10,13 +10,13 @@ def prompt
 end
 def magic_number(input)
   length = input.to_words.length
-  puts "#{input} is #{length.to_words}"
+  puts "#{input.to_s.colorize(:green)} is #{length.to_words.colorize(:red)}"
   while length != 4
     input = length
     length = input.to_words.length
-    puts "#{input} is #{length.to_words}"
+    puts "#{input.to_s.colorize(:green)} is #{length.to_words.colorize(:red)}"
   end
-  puts "#{length} is the magic number"
+  puts "#{length.to_s.colorize(:magenta)} is the magic number"
   prompt 
 end
     

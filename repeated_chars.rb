0f7b75777b4b7bfdeaf9eb01b89a6607e7444408
Jake Bills <jakebills1@gmail.com>
2019-04-12 09:59:44 -0600

    added more brainteaser answers

--- repeated_chars.rb
+++ repeated_chars.rb
@@ -2,9 +2,11 @@ def repeated_chars(str)
   results = []
   str_arr = str.split("")
   str_arr.each do |c|
+    if c != " "
      if str_arr.count(c) >= 2
       results.push(c)
      end
+    end
   end
   return results.uniq
 end
@@ -12,4 +14,8 @@ end
 
 # puts repeated_chars("abcde1234")
 # puts repeated_chars("aabcedde23562")
-puts repeated_chars("accDbjD3510")
+# puts repeated_chars("accDbjD3510")
+# puts repeated_chars("hello world")
+# puts repeated_chars("ds4ajk 2dsak wq3op d2sanjk")
+puts repeated_chars("aaaaaaaaaa")
+
